dbaaca91ef2edeedf0152c22ee106e7b2896721f
Olena Hryhorzhevska <elenahry8@gmail.com>
2026-01-09 19:56:50 -0500

    back keys added

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,14 +5,14 @@ import { getDatabase } from "firebase/database";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCy6x5nq0ZTazUxNB_khax86mkBsBqluwo",
-  authDomain: "nanny-app-5249c.firebaseapp.com",
-  databaseURL: "https://nanny-app-5249c-default-rtdb.firebaseio.com",
-  projectId: "nanny-app-5249c",
-  storageBucket: "nanny-app-5249c.firebasestorage.app",
-  messagingSenderId: "470996786028",
-  appId: "1:470996786028:web:f5eefd2119409dfb6f3149",
-  measurementId: "G-SD46SFLYC7",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
