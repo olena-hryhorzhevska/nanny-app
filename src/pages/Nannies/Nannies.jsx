388966803e19b0a2786eb8331a197678d2a7e8ce
Olena Hryhorzhevska <elenahry8@gmail.com>
2026-01-13 22:11:45 -0500

    nannies page card

--- src/pages/Nannies/Nannies.jsx
+++ src/pages/Nannies/Nannies.jsx
@@ -1,13 +1,46 @@
 import AppHeader from "../../components/AppHeader/AppHeader";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import LoginModal from "../../components/LoginModal/LoginModal.jsx";
 import RegistrationModal from "../../components/RegistrationModal/RegistrationModal.jsx";
 import { useAuthUser } from "../../hooks/useAuthUser.js";
+import { ref, get } from "firebase/database";
+import { db } from "../../firebase/firebase";
+import NannyCard from "../../components/NannyCard/NannyCard.jsx";
 
 export default function Nannies() {
   const { user, loading } = useAuthUser();
   const [isLoginOpen, setIsLoginOpen] = useState(false);
   const [isRegisterOpen, setIsRegisterOpen] = useState(false);
+
+  const [nannies, setNannies] = useState([]);
+  const [nanniesLoading, setNanniesLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNannies = async () => {
+      try {
+        const snap = await get(ref(db, "nannies"));
+
+        if (!snap.exists()) {
+          setNannies([]);
+          return;
+        }
+
+        const data = snap.val();
+        const list = Object.entries(data).map(([id, nanny]) => ({
+          id,
+          ...nanny,
+        }));
+
+        setNannies(list);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setNanniesLoading(false);
+      }
+    };
+
+    fetchNannies();
+  }, []);
 
   if (loading) return null;
 
@@ -22,8 +55,11 @@ export default function Nannies() {
           setIsRegisterOpen(true);
         }}
       />
-      <h1>Nannies Page</h1>
-      <p>This page provides information about nannies.</p>
+      {nanniesLoading ? (
+        <p>Loading nannies…</p>
+      ) : (
+        nannies.map((nanny) => <NannyCard key={nanny.id} nanny={nanny} />)
+      )}
       <LoginModal isOpen={isLoginOpen} onClose={() => setIsLoginOpen(false)} />
 
       <RegistrationModal
